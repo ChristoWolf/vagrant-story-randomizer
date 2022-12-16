c2565077efef918dff8f4b871b64b3b7cfbd82ea
ChristoWolf <christoph.n.wolf@gmail.com>
2022-12-16 11:58:17 +0100

    chore: covered error handling behavior of the `Execute` function in case the command execution failed (#26)

--- internal/psximager/psxinject_test.go
+++ internal/psximager/psxinject_test.go
@@ -3,6 +3,7 @@ package psximager_test
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/christowolf/vagrant-story-randomizer/internal/psximager"
@@ -19,6 +20,23 @@ func TestNewInjectCmd(t *testing.T) {
 	got := psximager.NewInjectCmd(wantPwd, wantArgs[0], wantArgs[1], wantArgs[2])
 	if !reflect.DeepEqual(*got, *want) {
 		t.Errorf("got: %v, want: %v", *got, *want)
+	}
+}
+
+// TestExecuteError tests the error handling
+// behavior of the Execute function
+// in case the command execution failed.
+func TestExecuteError(t *testing.T) {
+	t.Parallel()
+	wrongPath := "./non-existent-path"
+	err := psximager.Execute(wrongPath, "cueFile.cue", "orgFile", "injectFile")
+	if err == nil {
+		t.Errorf("expected a non-nil error")
+	}
+	got := err.Error()
+	want := fmt.Sprintf("error executing %s: exit status 1: '%s' is not recognized", psximager.PsxInject, wrongPath)
+	if strings.HasPrefix(got, want) {
+		t.Errorf("got: %v, want substring: %v", got, want)
 	}
 }
 
